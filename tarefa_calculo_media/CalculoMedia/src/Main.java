19b75fa3c8755b1e62acda4bf27291208d9a0238
juliagoncaalves <juliagoncalves_2@outlook.com>
2023-07-06 12:31:51 -0300

    tarefa calculo media

---
+++ tarefa_calculo_media/CalculoMedia/src/Main.java
@@ -0,0 +1,11 @@
+import media.CalculoMedia;
+public class Main {
+    public static void main(String[] args){
+
+        CalculoMedia calcmed = new CalculoMedia();
+        float resultado = calcmed.aritmetica(8,7,5,9);
+
+        System.out.println("O resultado da media: " + resultado);
+
+    }
+}
---
+++ tarefa_calculo_media/CalculoMedia/src/media/CalculoMedia.java
@@ -0,0 +1,11 @@
+package media;
+
+public class CalculoMedia {
+
+    public float aritmetica (float nota1, float nota2, float nota3, float nota4) {
+        float somaDasNotas = nota1 + nota2 + nota3 + nota4;
+        float mediaAritmetica = somaDasNotas / 4;
+
+        return mediaAritmetica;
+    }
+}
