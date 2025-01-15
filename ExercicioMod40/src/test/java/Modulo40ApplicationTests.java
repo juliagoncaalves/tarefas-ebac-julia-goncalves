7b8c5bf3bfab40645c8b268f073b0eadddd7a853
juliagoncaalves <juliagoncalves_2@outlook.com>
2025-01-15 20:09:50 -0300

    exercicio modulo 40

---
+++ ExercicioMod40/src/test/java/Modulo40ApplicationTests.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module40ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
